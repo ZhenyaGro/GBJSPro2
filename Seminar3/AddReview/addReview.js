ee580ea18c6de1b68fb71ef86f14a6759df806d9
ZhenyaGro <bon4ios@gmail.com>
2024-01-02 18:08:37 +0300

    Add fast fill function

--- Seminar3/AddReview/addReview.js
+++ Seminar3/AddReview/addReview.js
@@ -21,6 +21,27 @@ buttonAddEl.addEventListener('click', () => {
   }, 0);
 });
 
+document.querySelector('.review__button-fast-fill').addEventListener('click', () => {
+  const initialData = {
+    'Apple iPhone 13': {
+      '1': 'Отличный телефон! Батарея держится долго.',
+      '2': 'Камера супер, фото выглядят просто потрясающе.'
+    },
+    'Samsung Galaxy Z Fold 3': {
+      '3': 'Интересный дизайн, но дорогой.'
+    },
+    'Sony PlayStation 5': {
+      '4': 'Люблю играть на PS5, графика на высоте.'
+    }
+  }
+  try {
+    localStorage.setItem('reviews', JSON.stringify(initialData));
+    alert('LocalStorage заполнен, можно посмотреть отзывы на странице просмотра');
+  } catch (e) {
+    alert('Не удалось заполнить LocalStorage, проверьте сообщение в консоли');
+    console.log(e.message);
+  }
+})
 
 function saveReviewToLocalStorage(product, review) {
   let products = JSON.parse(localStorage.getItem('reviews')) || {};
